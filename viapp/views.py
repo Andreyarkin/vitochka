c19d340dda72d4e235fca53721109bcb93aca23e
Andreyarkin <andrey.yarkin24@gmail.com>
2025-03-14 14:36:07 +0300

    настроил безопасный доступ в проекте

--- viapp/views.py
+++ viapp/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.http import Http404
 from django.core.exceptions import PermissionDenied
+from django.contrib.auth.decorators import user_passes_test
 
 
 from .models import Album, Photo
 from .forms import AlbumForm, PhotoForm
+
+# проверка того, является ли пользователь администратором
+def admin_required(view_func):
+    """Декоратор для проверки прав суперпользователя."""
+    def wrapper(request, *args, **kwargs):
+        if not request.user.is_superuser:
+            raise PermissionDenied("Доступ запрещён.")
+        return view_func(request, *args, **kwargs)
+    return wrapper
 
 # Create your views here.
 def index(request):
@@ -15,7 +24,10 @@ def index(request):
 @login_required()
 def albums(request):
 	# выводит список альбомов
-	albums = Album.objects.filter(owner = request.user).order_by('-created_at')
+	if request.user.is_superuser:
+		albums = Album.objects.all().order_by('-created_at')
+	else:
+		albums = Album.objects.filter(owner = request.user).order_by('-created_at')
 	context = {'albums': albums}
 	return render(request, 'viapp/albums.html', context)
 
@@ -23,8 +35,8 @@ def albums(request):
 def album(request, album_id):
 	# страница альбома. Выводит альбом и фотографии в нем содержащиеся
 	album = Album.objects.get(id = album_id)
-	# Проверка того, что тема принадлежит текущему пользователю.
-	if album.owner != request.user:
+	# Проверка того, что альбом принадлежит текущему пользователю.
+	if not request.user.is_superuser and album.owner != request.user:
 		raise PermissionDenied()
 	photo = album.photos.order_by('-uploaded_at')
 	context = {'album':album, 'photo':photo}
@@ -34,10 +46,16 @@ def album(request, album_id):
 def photo(request, photo_id):
 	#страница фотографии
 	photo = Photo.objects.get(id = photo_id)
+	# Проверка того, что фото принадлежит текущему пользователю.
+	if not request.user.is_superuser and photo.album.owner != request.user:
+		raise PermissionDenied()
 	context = {'photo':photo}
 	return render(request, 'viapp/photo.html', context)
 
+# функция добавления альбома
+
 @login_required()
+@admin_required
 def add_album(request):
 	# Страница добавления альбома
 	if request.method != 'POST':
@@ -52,7 +70,9 @@ def add_album(request):
 	context = {'form' : form}
 	return render(request, 'viapp/add_album.html', context)
 
+# функция добавления фотографии
 @login_required()
+@admin_required
 def add_photo(request):
 	# Страница добавления фотографии
 	if request.method != 'POST':
@@ -60,12 +80,16 @@ def add_photo(request):
 	else:
 		form = PhotoForm(data = request.POST, files=request.FILES)
 		if form.is_valid():
-			photo = form.save()
-			return redirect ('viapp:album', album_id=photo.album.id)
+			photo_add = form.save()
+			photo_add.album.owner = request.user
+			photo_add.save()
+			return redirect ('viapp:album', album_id=photo_add.album.id)
 		else:
 			print(form.errors)
 	context = {'form' : form}
 	return render(request, 'viapp/add_photo.html', context)
+
+
 
 
 
