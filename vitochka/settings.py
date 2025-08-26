21b53458b55df814a77513122577fbaa21d56e58
Andreyarkin <andrey.yarkin24@gmail.com>
2025-08-26 13:23:59 +0300

    добавил static_root в settings.py

--- vitochka/settings.py
+++ vitochka/settings.py
@@ -134,6 +134,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",  # Подключаем папку static
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
