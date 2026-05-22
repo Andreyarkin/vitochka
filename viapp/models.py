009b77bb7e0bca2e18f76baa9999c90d4a8f4e0f
Andreyarkin <andrey.yarkin24@gmail.com>
2026-05-22 13:31:02 +0300

    добавил Nginx и валидацию по типу файла при загрузке фотографии в models.py

--- viapp/models.py
+++ viapp/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 from django.conf import settings
+from django.core.exceptions import ValidationError
+
+def validate_image(file):
+	if not hasattr(file, 'content_type'):
+		return
+	
+	if not file.content_type.startswith('image/'):
+		raise ValidationError("Можно загружать только изображения")
+
+def validate_size(file):
+	limit = 20 * 1024 * 1024
+	if file.size > limit:
+		raise ValidationError("Файл слишком большой")
 
 
 class Album(models.Model):
@@ -30,10 +43,15 @@ class Photo(models.Model):
 	title = models.CharField(max_length=100, null=True, blank=True, verbose_name="Название фотографии")
 	description = models.TextField(blank=True, verbose_name="Описание фотографии")
 	album = models.ForeignKey(Album, related_name="photos", on_delete=models.CASCADE, verbose_name="Альбом")
-	image = models.ImageField(upload_to='photos/', verbose_name=" Фотография")
+	image = models.ImageField(
+		upload_to='photos/',
+		verbose_name=" Фотография",
+		validators = [validate_image, validate_size]
+	)
 	caption = models.CharField(max_length=255, blank=True, null=True, verbose_name="Подпись")
 	uploaded_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата загрузки")
 	order = models.PositiveIntegerField(default=0, verbose_name="Порядок")
+	
 
 	def __str__(self):
 		return f"{self.title} - альбом: {self.album.title}"
